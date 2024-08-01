d40fa197a4ae8963f11b1c799fc0f9a35038d355
Sanath Kumar <sanath.kumar@kenko-health.in>
2024-08-02 01:55:04 +0530

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-mainTextColor text-white">
+      <div className="max-w-7xl mx-auto py-5 px-4 sm:px-6 lg:px-8">
+        <p className="text-center">© Copyright 2024. Bhive Private Limited</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
